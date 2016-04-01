c13ff4fc0ed9a2065780f2c987e5bfbf43a1b0dd
David Shorthouse <davidpshorthouse@gmail.com>
2016-04-01 09:22:48 -0400

    Use config.yml to select dataset stats to download

--- bin/stats.rb
+++ bin/stats.rb
@@ -2,49 +2,57 @@
 # encoding: utf-8
 require_relative '../environment.rb'
 
-ARGV << '-h' if ARGV.empty?
-
 options = {}
 
-uuids = {
-  herbarium: '830da118-f762-11e1-a439-00145eb45e9a',
-  mollusc: '830c7b08-f762-11e1-a439-00145eb45e9a',
-  amphibian_reptile: '830a1f84-f762-11e1-a439-00145eb45e9a',
-  bird: '8309005e-f762-11e1-a439-00145eb45e9a',
-  fish: '830b4af8-f762-11e1-a439-00145eb45e9a'
-}
-
-OptionParser.new do |opts|
+optparse = OptionParser.new do |opts|
   opts.banner = "Usage: stats.rb [options]"
 
   opts.on("-h", "--help", "Prints this help") do
     puts opts
     exit
   end
-
-  opts.on("-a", "--all", "Calculates all stats") do |a|
-    options[:all] = true
-  end
   
-  opts.on("-d", "--uuid [UUID]", String, "Include the UUID of the dataset") do |uuid|
-    options[:uuid] = uuid
+  opts.on("-c", "--config [FILE]", String, "Include a full path to the config.yml file") do |config|
+    options[:config] = config
   end
 
-end.parse!
+  opts.on("-o", "--output [DIRECTORY]", String, "Directory for the output files") do |directory|
+    options[:output_dir] = directory
+  end
+end
 
-if options[:all]
-  pbar = ProgressBar.new("STATS", uuids.count)
-  output_dir = File.dirname(File.dirname(__FILE__)) + "/output/"
+begin
+  optparse.parse!
+  if options[:config]
+    config_file = options[:config]
+  else
+    config_file = File.join(File.dirname(File.dirname(__FILE__)), 'config.yml')
+  end
+  raise "File not found" unless File.exists?(config_file)
+
+  if options[:output_dir]
+    output_dir = options[:output_dir]
+  else
+    output_dir = File.join(File.dirname(File.dirname(__FILE__)), "output")
+  end
+  raise "Directory not found" unless Dir.exists?(output_dir)
+
+  config = YAML.load_file(config_file)
+  pbar = ProgressBar.new("STATS", config.size)
   counter = 0
-  uuids.each do |key,uuid|
+  config.each do |item|
+    key = item[0]
+    uuid = item[1]["uuid"]
+    numlines = item[1]["lines"]
+
     counter += 1
     pbar.set(counter)
     response = RestClient::Request.execute(
       method: :get,
-      url: "http://api.gbif.org/v1/occurrence/download/dataset/#{uuid}?offset=0&limit=500",
+      url: "http://api.gbif.org/v1/occurrence/download/dataset/#{uuid}?offset=0&limit=#{numlines}",
     )
     results = JSON.parse(response, :symbolize_names => true)[:results]
-    CSV.open(output_dir + "gbif_cmn_stats_#{key}_#{Date.today.to_s}.csv", 'w') do |csv|
+    CSV.open(File.join(output_dir, "gbif_stats_#{key}.csv"), 'w') do |csv|
       csv << ["download_num_records", "download_doi", "created"]
       results.each do |result|
         download_num_records = result[:numberRecords]
@@ -58,6 +66,8 @@ if options[:all]
     end
   end
   pbar.finish
-elsif options[:uuid]
-  puts "Sorry, not yet implemented"
+rescue
+  puts $!.to_s
+  puts optparse
+  exit 
 end
